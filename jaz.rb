84d210c5bb133a8ffac0267c9207f4d3ec53d932
John Naylor <jonaylor89@gmail.com>
2019-11-26 23:50:41 -0500

    bottle and binary

--- jaz.rb
+++ jaz.rb
@@ -1,29 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Jaz < Formula
   desc "Find secrets hidden in commits"
-  homepage ""
+  homepage "https://github.com/jonaylor89/JAZ"
   url "https://github.com/jonaylor89/JAZ/archive/v0.0.2.tar.gz"
   sha256 "2474927a922c5e913d65473fd0f850eb09e8f6efaa5d553b99d6aa095f479054"
 
-  depends_on "rust" => :build
+  if OS.mac?
+    url "https://github.com/jonaylor89/JAZ/releases/download/v0.0.2/jaz"
+    sha256 "32754b4173ac87a7bfffd436d601a49362676eb1841ab33440f2f49c002c8967"
+  end
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    bin.install "jaz"
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test JAZ`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
