bfd46d09922f63ae6989092aeaaf129eaca0db89
John Naylor <jonaylor89@gmail.com>
2019-11-26 20:37:47 -0500

    add jaz

---
+++ jaz.rb
@@ -0,0 +1,29 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class jaz < Formula
+  desc "Find secrets hidden in commits"
+  homepage ""
+  url "https://github.com/jonaylor89/JAZ/archive/v0.1.0.tar.gz"
+  sha256 "419103745eed49d537f3e73370d3274f0259b00c33ebc34af69eefdc70a48a8e"
+
+  depends_on "rust" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test JAZ`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
